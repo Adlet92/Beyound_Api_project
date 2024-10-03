eb584be0c236e022e88ca44bdde841b8f256ee8d
adletb7 <adlet.baiteliyev@gmail.com>
2024-10-03 17:38:53 +0500

    api-auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { Pages } from "@pages/index";
 import { router } from "@shared/config/routes";
 import { RouterProvider } from "atomic-router-react";
 import "./index.css";
+import { Pages } from "./pages/index";
 
 function App() {
   return (
